a81cd02fab734f026ecada971dfe3d7cea00489d
Jakub Kielar <jacob.kielar@gmail.com>
2025-04-05 13:38:58 +0200

    providing the world map with the print in console

--- StreetSimulatorApp.cpp
+++ StreetSimulatorApp.cpp
@@ -9,11 +9,20 @@
 #include "StreetSimulatorPresenter.h"
 #include "StreetSimulatorSFMLView.h"
 #include "StreetSimulatorView.h"
+#include "WorldMapGridProvider.h"
 
 StreetSimulatorApp::StreetSimulatorApp() {
     auto window = new sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "StreetSimulator");
 
     StreetSimulatorView *view = new StreetSimulatorSFMLView(window);
+    auto *provider = new WorldMapGridProvider("road1.txt");
+    auto grid = provider->provideGrid();
+    for (auto a : grid) {
+        for (auto b: a) {
+            std::cout<<b<<" ";
+        }
+        std::cout<<std::endl;
+    }
     StreetSimulatorPresenter *presenter = new StreetSimulatorPresenter(view);
 
     while (window->isOpen()) {
---
+++ WorldMapGridProvider.cpp
@@ -0,0 +1,40 @@
+
+#include "WorldMapGridProvider.h"
+
+#include <array>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <sstream>
+
+#include "Point.h"
+
+GridType WorldMapGridProvider::provideGrid() {
+    return grid;
+}
+
+WorldMapGridProvider::WorldMapGridProvider(const std::string &filename) {
+    readGridFromFile(filename);
+}
+
+
+void WorldMapGridProvider::readGridFromFile(const std::string &filename) {
+    GridType gridMap{};
+    std::ifstream file("road1.txt");
+
+    if (!file) {
+        throw std::runtime_error("Unable to open file: " + filename);
+    }
+
+    std::string line;
+    int rowIdx = 0;
+
+    while (std::getline(file, line) && rowIdx < ROWS) {
+        std::stringstream ss(line);
+        for (int colIdx = 0; colIdx < COLS; ++colIdx) {
+            ss >> gridMap[rowIdx][colIdx];
+        }
+        ++rowIdx;
+    }
+    grid = gridMap;
+}
